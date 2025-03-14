676750cb38f947dcd44cad8d4086d484ba87af63
root <sissonjack67@gmail.com>
2025-03-14 05:29:46 +0000

    feat: barbones site

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
+  <!--<header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
-  </header>
+  </header>-->
 
   <main>
-    <TheWelcome />
+    <div class="header-section" >
+      <div class="header-text">
+        Jack Sisson
+      </div>
+    </div>
+    <div class="menu-section">
+      <div class="menu-item">
+        Photography
+      </div>
+      <div class="menu-item">
+        Engineering
+      </div>
+      <div class="menu-item">
+        Blog
+      </div>
+    </div>
   </main>
 </template>
 
@@ -43,5 +57,55 @@ header {
     place-items: flex-start;
     flex-wrap: wrap;
   }
+}
+
+html, body, #app {
+  height: 100vh;
+  width: 100vw;
+  margin: 0;
+  padding: 0;
+  background-color: #f8ede2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+main {
+  background-color: #f8ede2;
+  color: black;
+  width: 100vw;
+  height: 100vh;
+
+  .header-section {
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    margin-top: 100px;
+
+    .header-text {
+      font-size: 34px;
+      font-weight: 700;
+      margin: 30px;
+    }
+  }
+
+  .menu-section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: left;
+    margin-top: 50px;
+
+    .menu-item {
+      font-size: 24px;
+      font-weight: 600;
+      margin: 30px;
+
+      &:hover {
+        font-size: 27px;
+      }
+    }
+  }
+
 }
 </style>
